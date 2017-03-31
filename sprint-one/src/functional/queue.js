eb83b644f657543546dcf4659592fefbf6583d57
Kara Marnell <kmarnell@gmail.com>
2017-03-30 19:14:04 -0700

    functional tests pasts for stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,30 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {};  
+  var count = 0; 
+  var position = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-  };
+    count++;
+    position++;
+    storage[position] = value;
+  }
 
   someInstance.dequeue = function() {
+    if(count > 0) {
+      var arrayOfKeys = Object.keys(storage)   //['1'...]
+      var removed = storage[arrayOfKeys[0]];
+      delete storage[arrayOfKeys[0]];
+      count--;  
+    }
+    return removed;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -8,14 +8,22 @@ var Stack = function() {
   // Implement the methods below
   someInstance.push = function(value) {
     count++;
+    storage[count] = value; 
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function() {  
+    var popped = storage[count];
+    delete storage[count]; 
+    if(count > 0) { 
+      count--;
+    }
+    return popped;
   };
 
   someInstance.size = function() {
     return count;
   };
 
-  return someInstance;
+  
+  return someInstance; 
 };
