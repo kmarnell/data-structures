3dbbdc9af611134ac7c253fb0fc2af2cfbd3cae1
Kara Marnell <kmarnell@gmail.com>
2017-03-30 19:15:36 -0700

    stack functional-shared tests pass

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-};
+	var someInstance = {};
+	someInstance.storage = {};
+	someInstance.count = 0;
+	_.extend(someInstance, stackMethods);
+  return someInstance;
+}
+
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+	this.count++;
+	this.storage[this.count] = value;
+}
 
+stackMethods.pop = function() {
+	var removed = this.storage[this.count];
+	if(this.count > 0) {
+		delete this.storage[this.count];
+		this.count--;
+		}
+		return removed;
+	}
+
+stackMethods.size = function() {
+	return this.count;
+}
