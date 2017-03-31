4474e6d662b38eed9f970ea2db0e8d9b4a912c55
Kara Marnell <kmarnell@gmail.com>
2017-03-30 21:02:53 -0700

    set tests pass

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {};
+  set.keys = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	this.keys++;
+	this.storage[this.keys] = item;
 };
 
 setPrototype.contains = function(item) {
+	for(var key in this.storage) {
+		if(this.storage[key] === item) {
+			return true;
+		}
+	}
+	return false;
 };
 
 setPrototype.remove = function(item) {
+	for(var key in this.storage) {
+		if(this.storage[key] === item) {
+			delete this.storage[key];
+		}
+	}
 };
 
 /*
