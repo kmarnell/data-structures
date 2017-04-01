d4348ec109758915d59123db734ff8719ff3cc9e
Kara Marnell <kmarnell@gmail.com>
2017-04-01 14:24:21 -0700

    passing tests for linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,45 @@
 var LinkedList = function() {
-  var list = {};
+  var list = {}
   list.head = null;
   list.tail = null;
 
+
+
   list.addToTail = function(value) {
+    
+    //if the list is empty, add a value and point it to the tail
+    if(list.tail === null) {
+      list.tail = Node(value);
+      list.head = list.tail
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    if(list.head === null) {
+      return null;
+    } else {
+      var removed = list.head.value;
+      var newHead = list.head.next;
+      delete list.head
+      list.head = newHead;
+    }
+    return removed;
   };
 
   list.contains = function(target) {
+    //if head value has target, return true
+    var current = list.head;
+    while(current !== null) {
+      if(current.value === target) {
+        return true;
+      } else {
+        current = current.next;
+      } 
+    }  
+    return false;
   };
 
   return list;
